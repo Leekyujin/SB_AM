8c30635a289960f2d9d92de199d0c99c93d1f3b2
Leekyujin <kyujinlee82@gmail.com>
2022-09-26 16:48:12 +0900

    게시물 테스트데이터 10개 생성

--- src/main/java/com/lkj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/lkj/exam/demo/controller/UsrArticleController.java
@@ -18,8 +18,23 @@ public class UsrArticleController {
 	public UsrArticleController() {
 		lastArticleId = 0;
 		articles = new ArrayList<>();
+		
+		makeTestData();
 	}
 	
+	public void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			int id = lastArticleId + 1;
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+			
+			Article article = new Article(id, title, body);
+			
+			articles.add(article);
+			lastArticleId= id;
+		}
+	}
+	 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
@@ -37,4 +52,19 @@ public class UsrArticleController {
 	public List<Article> getArticles() {
 		return articles;
 	}
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public List<Article> doDelete(int id) {
+		
+		
+		return articles;
+	}
+	
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public List<Article> doModify(int id) {
+		return articles;
+	}
+	
 }
