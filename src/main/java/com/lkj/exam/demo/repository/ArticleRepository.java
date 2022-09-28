8a43b996b6031663b49034a61c8a02e3ecbd1c37
Leekyujin <kyujinlee82@gmail.com>
2022-09-28 17:08:10 +0900

    articleModify 시 입력된 정보만 수정될 수 있도록

--- src/main/java/com/lkj/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/lkj/exam/demo/repository/ArticleRepository.java
@@ -2,11 +2,7 @@ package com.lkj.exam.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.lkj.exam.demo.vo.Article;
 
