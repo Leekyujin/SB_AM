d3ade0bbb9c36d5a21505bdecc7330cb72a58138
Leekyujin <kyujinlee82@gmail.com>
2022-09-30 17:46:22 +0900

    usr/member/doJoin에 ResultData 적용

--- src/main/java/com/lkj/exam/demo/controller/UsrMemberController.java
+++ src/main/java/com/lkj/exam/demo/controller/UsrMemberController.java
@@ -21,40 +21,33 @@ public class UsrMemberController {
 			String nickname, String cellphoneNum, String email) {
 		
 		if (Ut.empty(loginId)) {
-			return ResultData.from("F-3", Ut.f("아이디를 입력해주세요."));
+			return ResultData.from("F-1", "아이디를 입력해주세요.");
 		}
 		if (Ut.empty(loginPw)) {
-			return ResultData.from("F-4", Ut.f("비밀번호를 입력해주세요."));
+			return ResultData.from("F-2", "비밀번호를 입력해주세요.");
 		}
 		if (Ut.empty(name)) {
-			return ResultData.from("F-5", Ut.f("이름을 입력해주세요."));
+			return ResultData.from("F-3", "이름을 입력해주세요.");
 		}
 		if (Ut.empty(nickname)) {
-			return ResultData.from("F-6", Ut.f("닉네임을 입력해주세요."));
+			return ResultData.from("F-4", "닉네임을 입력해주세요.");
 		}
 		if (Ut.empty(cellphoneNum)) {
-			return ResultData.from("F-7", Ut.f("전화번호를 입력해주세요."));
+			return ResultData.from("F-5", "전화번호를 입력해주세요.");
 		}
 		if (Ut.empty(email)) {
-			return ResultData.from("F-8", Ut.f("이메일을 입력해주세요."));
+			return ResultData.from("F-6", "이메일을 입력해주세요.");
 		}
 		
-		int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
-		// resultCode
-		// msg
-		// data1
+		ResultData joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNum, email);
 
-		if (id == -1) {
-			return ResultData.from("F-1", Ut.f("%s은(는) 이미 사용중인 아이디입니다.", loginId));
+		if (joinRd.isFail()) {
+			return joinRd;
 		}
 		
-		if (id == -2) {
-			return ResultData.from("F-2", Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다.", name, email));
-		}
+		Member member = memberService.getMemberById((int) joinRd.getData1());
 		
-		Member member = memberService.getMemberById(id);
-		
-		return ResultData.from("S-1", Ut.f("%s 회원님 환영합니다.", name), member);
+		return ResultData.newData(joinRd, member);
 	}
 
 }
--- src/main/java/com/lkj/exam/demo/service/MemberService.java
+++ src/main/java/com/lkj/exam/demo/service/MemberService.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lkj.exam.demo.repository.MemberRepository;
+import com.lkj.exam.demo.util.Ut;
 import com.lkj.exam.demo.vo.Member;
+import com.lkj.exam.demo.vo.ResultData;
 
 @Service
 public class MemberService {
@@ -15,24 +17,26 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 	}
 
-	public int join(String loginId, String loginPw, String name, String nickname,
+	public ResultData join(String loginId, String loginPw, String name, String nickname,
 			String cellphoneNum, String email) {
 		// 로그인아이디 중복체크
 		Member existsMember = getMemberByLoginId(loginId);
 		
 		if (existsMember != null) {
-			return -1;
+			return ResultData.from("F-7", Ut.f("%s은(는) 이미 사용중인 아이디입니다.", loginId));
 		}
 		
 		// 이름 + 이메일 중복체크
 		existsMember = getMemberByNameAndEmail(name, email);
 		
 		if (existsMember != null) {
-			return -2;
+			return ResultData.from("F-8", Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다.", name, email));
 		}
 		
 		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNum, email);
-		return memberRepository.getLastInsertId();
+		int id = memberRepository.getLastInsertId();
+		
+		return ResultData.from("S-1", "회원가입이 완료되었습니다.", id);
 	}
 
 	private Member getMemberByNameAndEmail(String name, String email) {
