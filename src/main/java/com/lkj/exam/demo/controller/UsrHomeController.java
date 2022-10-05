ea56c2453470b1c4d94e171a79d4c2c353030a9b
Leekyujin <kyujinlee82@gmail.com>
2022-10-05 18:44:54 +0900

    게시물 리스트 JSP로 구현

--- src/main/java/com/lkj/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/lkj/exam/demo/controller/UsrHomeController.java
@@ -2,14 +2,17 @@ package com.lkj.exam.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
-	public String getString() {
-		return "hi";
+	public String showMain() {
+		return "usr/home/main";
+	}
+
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 }
 
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인</title>
+</head>
+<body>
+	<h1>메인 페이지</h1>
+	<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla suscipit error labore recusandae porro aliquam officia sed a voluptate voluptatibus quas aut nam magnam officiis quasi expedita nihil? Sint facilis.</div>
+</body>
+</html>
